e14e15c4f6a62b253da26279e7cc616e19caa704
Jakob Kaivo <jkk@ung.org>
2019-08-06 08:50:17 -0400

    clean up

--- link.c
+++ link.c
@@ -1,23 +1,28 @@
 /*
  * UNG's Not GNU
  *
- * Copyright (c) 2011-2017, Jakob Kaivo <jkk@ung.org>
+ * Copyright (c) 2011-2019, Jakob Kaivo <jkk@ung.org>
  *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
-#define _XOPEN_SOURCE 700
+#define _POSIX_C_SOURCE 2
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
@@ -29,14 +34,19 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	if (optind != argc - 2) {
-		fprintf(stderr, "link: Must specify a source and destination\n");
+	if (argc - optind > 2) {
+		fprintf(stderr, "link: extra operands\n");
+		return 1;
+	}
+
+	if (argc - optind < 2) {
+		fprintf(stderr, "link: missing operands\n");
 		return 1;
 	}
 
 	if (link(argv[optind], argv[optind + 1]) != 0) {
-		fprintf(stderr, "link: Couldn't link '%s' to '%s': %s\n",
-			argv[optind], argv[optind + 1], strerror(errno));
+		fprintf(stderr, "link: %s -> %s: %s\n", argv[optind],
+			argv[optind + 1], strerror(errno));
 		return 1;
 	}
 
